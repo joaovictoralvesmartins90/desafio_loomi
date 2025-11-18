bab407c7a2344db934781e53a162f471e7c2d1e4
João Víctor Alves Martins <jvam90@proton.me>
2025-11-18 18:50:52 -0300

    commit inicial

---
+++ desafio_loomi/src/main/java/com/example/desafio_loomi/DesafioLoomiApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_loomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLoomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLoomiApplication.class, args);
+	}
+
+}
---
+++ desafio_loomi/src/test/java/com/example/desafio_loomi/DesafioLoomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_loomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLoomiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
